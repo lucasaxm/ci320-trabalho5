011282fab3c72bdde737f8be7bca5017ac669191
Lucas Affonso Xavier De Morais <lucas_axm@hotmail.com>
2014-07-04 17:42:54 -0300

    first commit

---
+++ PokemonRuby/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PokemonRuby::Application.config.secret_key_base = 'b39ae2dc4c2b1862bb6b922194be50af7dedb3cced26478ebd42425ca2c5398d66e867dd41835df1a4f05814baad36d36840532204c3dd251ec83d0e438f4cdf'
---
+++ PokemonRuby/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PokemonRuby::Application.config.session_store :cookie_store, key: '_PokemonRuby_session'
