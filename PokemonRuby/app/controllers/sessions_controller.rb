26182daeee4a5b781dfc0076e1d275573aea6d61
Lucas Affonso Xavier De Morais <lucas_axm@hotmail.com>
2014-07-10 00:37:20 -0300

    log in implemented. starting to make log out.

--- PokemonRuby/app/controllers/sessions_controller.rb
+++ PokemonRuby/app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.authenticate(params[:login], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid login or password"
+      render "new"
+    end
+  end
 end
--- PokemonRuby/config/routes.rb
+++ PokemonRuby/config/routes.rb
@@ -1,8 +1,11 @@
 PokemonRuby::Application.routes.draw do
-  get "sessions/new"
-  get "sign_up" => "users#new", :as => "sign_up"  
-  root :to => "users#new"  
+  get "log_in" => "sessions#new", :as => "log_in"
+
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "sessions#new"
   resources :users  
+
+  resources :sessions
 
   resources :bags
 
